b19dba47396f3a7adfb11645bfba373fde29ae72
Federico Pereira <lord.basex@gmail.com>
2021-12-19 20:25:38 -0300

    Add files via upload

---
+++ aws/backend/main.tf
@@ -0,0 +1,84 @@
+resource "aws_s3_bucket" "state" {
+  bucket = "terraform-remote-state-${var.project_name}"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name    = "terraform-remote-state-${var.project_name}"
+    Project = var.project_name
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "access_block" {
+  bucket                  = aws_s3_bucket.state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "b" {
+  bucket = aws_s3_bucket.state.bucket
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "PutObjPolicy",
+  "Statement": [
+    {
+      "Sid": "DenyIncorrectEncryptionHeader",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "s3:PutObject",
+      "Resource": "${aws_s3_bucket.state.arn}/*",
+      "Condition": {
+        "StringNotEquals": {
+          "s3:x-amz-server-side-encryption": "AES256"
+        }
+      }
+    },
+    {
+      "Sid": "DenyUnEncryptedObjectUploads",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "s3:PutObject",
+      "Resource": "${aws_s3_bucket.state.arn}/*",
+      "Condition": {
+        "Null": {
+          "s3:x-amz-server-side-encryption": "true"
+        }
+      }
+    }
+  ]
+}
+EOF
+
+  depends_on = [aws_s3_bucket_public_access_block.access_block]
+}
+
+resource "aws_dynamodb_table" "terraform_state_locktable" {
+  name         = "terraform-remote-state-lock-${var.project_name}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name    = "terraform-remote-state-lock-${var.project_name}"
+    Project = var.project_name
+  }
+}
